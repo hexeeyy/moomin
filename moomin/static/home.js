17be92a3de6623753cf5a832c656fa33a48c030c
Mark Hexilon Payno <hexilonpayno@gmail.com>
2024-12-24 21:52:16 +0800

    Add files via upload

---
+++ moomin/static/home.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["moomin!", "bebii!", "seyii!", "baobeii!", "love!", "Genesis!"],
+        typeSpeed: 200,
+        backSpeed: 60,
+        loopCount: Infinity,
+        loop: true,
+        backDelay: 3000,
+    });
+});
